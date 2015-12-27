da51dd84c05942cc09639b3e57b83a75f4d5659a
Mohammed Talha <talhaqqq@gmail.com>
2015-12-27 12:40:25 +0530

    Seed data for Itemtypes and Jobtypes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,56 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Agent.create(name: 'Heroku Agent', contactname: 'Heroku  contact name' , contactno: 'heroku contactno' , othcontactno: 'heroku othcontactno' ,othrefdetails:'heroku oth ref details')
-Customer.create(name: 'Heroku Client', contactname: 'Heroku contact name' , contactno: 'heroku contactno' , othcontactno: 'heroku othcontactno', othrefdetails:'heroku oth ref details')
+# Agent.create(name: 'Heroku Agent', contactname: 'Heroku  contact name' , contactno: 'heroku contactno' , othcontactno: 'heroku othcontactno' ,othrefdetails:'heroku oth ref details')
+# Customer.create(name: 'Heroku Client', contactname: 'Heroku contact name' , contactno: 'heroku contactno' , othcontactno: 'heroku othcontactno', othrefdetails:'heroku oth ref details')
+Itemtype.create(name: '3D Fab. letter');
+Itemtype.create(name: 'Al. Light box');
+Itemtype.create(name: 'Uni-pole');
+Itemtype.create(name: 'Identity Sign');
+Itemtype.create(name: 'Custom Sign');
+Itemtype.create(name: 'Light box');
+Itemtype.create(name: 'Marketing Fence');
+Itemtype.create(name: 'Suspension Sign');
+Itemtype.create(name: 'Exhibit');
+Itemtype.create(name: 'Vinyl Crafting');
+Itemtype.create(name: 'Flex Sign');
+Itemtype.create(name: 'Elect. Maint.');
+Itemtype.create(name: 'Traffic Sign');
+Itemtype.create(name: 'sign Structure');
+Itemtype.create(name: 'CNC/Plotter Work');
+Itemtype.create(name: 'Corporate Sign');
+Itemtype.create(name: 'Pylon');
+Itemtype.create(name: 'Safety sign');
+Itemtype.create(name: 'Totem');
+Itemtype.create(name: 'LED Panel Sign');
+Itemtype.create(name: 'Site Project Sign');
+Itemtype.create(name: 'Display Stand');
+Itemtype.create(name: 'Acrylic Stand');
+Itemtype.create(name: 'ACP Background');
+Itemtype.create(name: 'Other (outdoor)');
+Itemtype.create(name: 'Flex Replace');
+Itemtype.create(name: 'Direction Sign');
+Itemtype.create(name: 'Sign Shifting');
+Itemtype.create(name: 'Height Sign');
+Itemtype.create(name: '3D Cut letter');
+Itemtype.create(name: 'Al. Profile Sign');
+Itemtype.create(name: 'Mopi');
+Itemtype.create(name: 'Push through');
+Itemtype.create(name: 'Clip frame');
+Itemtype.create(name: 'Hoarding Sign');
+Itemtype.create(name: 'ACP Box');
+Itemtype.create(name: 'Promotion Sign');
+Itemtype.create(name: 'Banner Sign');
+Itemtype.create(name: 'Sign Cleaning');
+Itemtype.create(name: 'Category Sign');
+Itemtype.create(name: 'Flag Sign');
+Itemtype.create(name: 'Other (indoor)');
+
+
+Jobtype.create(name: 'Production')
+Jobtype.create(name: 'Installation')
+Jobtype.create(name: 'Civil')
+Jobtype.create(name: 'Transport')
+Jobtype.create(name: 'Crane')
+Jobtype.create(name: 'Tools and Equipment / rentals')
+Jobtype.create(name: 'Others')
