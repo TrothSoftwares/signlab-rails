8c164f010163e1cd70e7686ad8959f7f002d167a
Mohammed Talha <talhaqqq@gmail.com>
2016-01-11 08:41:09 +0530

    authentication and authorization worked together

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,18 @@
 class ApplicationController < ActionController::Base
-  attr_reader :current_user
-    respond_to :json
-    before_action :authenticate_user_from_token!
-    protect_from_forgery with: :null_session
+  before_filter :authenticate_user_from_token!
 
-    private
+  before_filter :authenticate_user!
+
+  private
 
     def authenticate_user_from_token!
-      authenticated = authenticate_with_http_token do |user_token, options|
+      authenticate_with_http_token do |token, options|
         user_email = options[:email].presence
-        user       = user_email && User.find_by_email(user_email)
+        user = user_email && User.find_by_email(user_email)
 
-        if user && Devise.secure_compare(user.authentication_token, user_token)
-          @current_user = user
+        if user && Devise.secure_compare(user.authentication_token, token)
           sign_in user, store: false
-        else
-          render json: { errors: ['Invalid authorization.'] }, status: :unauthorized
         end
-      end
-
-      unless authenticated
-        render json: { errors: ['No authorization provided.'] }, status: :unauthorized
       end
     end
 end
