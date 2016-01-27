408e369ca364519e8b008e60a1209f1dd8b27dd4
Mohammed Talha <talhaqqq@gmail.com>
2016-01-27 11:21:15 +0530

    After converting enquiries to single enquiry

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -2,7 +2,7 @@ class ProjectSerializer < ActiveModel::Serializer
   attributes :id , :name , :location , :status ,:duedate
   belongs_to :customer
   belongs_to :agent
-  has_many :enquiries
+  has_one :enquiry
   has_many :items
   has_one :quotation
 end
